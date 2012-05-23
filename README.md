627da7d622205488d5001560f1d891e57b3f03eb
Delgermurun <delgermurun.p@nmma.co>
2012-05-23 19:51:02 +0800

    Answer 22

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-udacity-cs212
-=============
+CS212: Design of Computer Programs
+==================================
 
-CS212: Design of Computer Programs Improve your programming skills with new patterns and techniques.
+Improve your programming skills with new patterns and techniques.
+
+Course's code.
+
+Website:
+========
+
+http://www.udacity.com/overview/Course/cs212/CourseRev/apr2012
--- unit-3/22-n_ary.py
+++ unit-3/22-n_ary.py
@@ -8,19 +8,5 @@ def n_ary(f):
     """Given binary function f(x, y), return an n_ary function such
     that f(x, y, z) = f(x, f(y,z)), etc. Also allow f(x) = x."""
     def n_ary_f(x, *args):
-        if args:
-            args = (x,) + args
-            r = f(args[-2], args[-1])
-            args = list(args[:-2])
-            args.reverse()
-            for i in args:
-                r = f(i, r)
-            return r
-        else:
-            return x
+        return x if not args else f(x, n_ary_f(*args))
     return n_ary_f
-
-def l(x,y):
-    return x,y
-
-a = n_ary(l)
