97f5b74733a8b58ea7dfa8550c114f63afc9ab26
Delgermurun <delgermurun.p@nmma.co>
2012-05-23 15:29:56 +0800

    Answer 14

--- unit-3/source.py
+++ unit-3/source.py
@@ -9,7 +9,7 @@ def lit(s):         return lambda Ns: set([s]) if len(s) in Ns else null
 def alt(x, y):      return lambda Ns: x(Ns) | y(Ns)
 def star(x):        return lambda Ns: opt(plus(x))(Ns)
 def plus(x):        return lambda Ns: genseq(x, star(x), Ns, startx=1) #Tricky
-def oneof(chars):   return lambda Ns: set.union(set([]), *[s for s in [lit(ch)(Ns) for ch in chars] if s])
+def oneof(chars):   return lambda Ns: set(chars) if 1 in Ns else null
 def seq(x, y):      return lambda Ns: genseq(x, y, Ns)
 def opt(x):         return alt(epsilon, x)
 dot = oneof('?')    # You could expand the alphabet to more chars.
